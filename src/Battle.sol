6aeb001c7d2f1e17a56cdb8e023f8d8be7225a3f
ygouzerh <33725211+ygouzerh@users.noreply.github.com>
2025-02-16 19:50:57 +0800

    Initial commit

---
+++ src/Battle.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+contract Battle {
+
+    error Battle_NotEnoughInitialPayment();
+    error Battle_AlreadyHaveCharacter();
+    error Battle_AddressDoesntHaveCharacter();
+    error Battle_PaymentFailed();
+    error Battle_InvalidName();
+
+    struct Character {
+        string name;
+        uint8 health;
+        uint256 wealth;
+    }
+
+    uint256 immutable i_initialPaymentNeeded;
+    uint8 constant INITIAL_HEALTH = 100;
+
+    mapping (address => Character) characters;
+
+    constructor(uint256 _initialPaymentNeeded) {
+        i_initialPaymentNeeded = _initialPaymentNeeded;
+    }
+
+    function joinBattle(string calldata _name) payable external {
+        if (msg.value < i_initialPaymentNeeded) {
+            revert Battle_NotEnoughInitialPayment();
+        }
+        if (bytes(characters[msg.sender].name).length != 0) {
+            revert Battle_AlreadyHaveCharacter();
+        }
+        if (bytes(_name).length == 0) {
+            revert Battle_InvalidName();
+        }
+        // verify that address not already there
+        Character memory newCharacter = Character(_name, INITIAL_HEALTH, msg.value);
+        characters[msg.sender] = newCharacter;
+    }
+
+    // function eraseCharacter() external {
+    //     if (bytes(characters[msg.sender].name).length == 0) {
+    //         revert Battle_AddressDoesntHaveCharacter();
+    //     }
+    //     (bool sentSuccessfully, ) = payable(msg.sender).call{value: address(this).balance-characters[msg.sender].wealth}("");
+    //     require(sentSuccessfully, Battle_PaymentFailed());
+    // }
+
+    function withdrawWealth() external {
+        if (bytes(characters[msg.sender].name).length == 0) {
+            revert Battle_AddressDoesntHaveCharacter();
+        }
+        uint256 wealthToSent = characters[msg.sender].wealth;
+        characters[msg.sender].wealth = 0;
+        (bool sentSuccessfully, ) = payable(msg.sender).call{value: wealthToSent}("");
+        require(sentSuccessfully, Battle_PaymentFailed());
+    }
+
+    function getCharacterName(address _address) checkCharacterExists(_address) public view returns (string memory) {
+        return characters[_address].name;
+    }
+
+    function getCharacterWealth(address _address) checkCharacterExists(_address) public view returns (uint256) {
+        return characters[_address].wealth;
+    }
+
+    modifier checkCharacterExists(address _address) {
+        string memory name = characters[_address].name;
+        if (bytes(name).length == 0) {
+            revert Battle_AddressDoesntHaveCharacter();
+        }
+        _;
+    }
+}
---
+++ test/Battle.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {Battle} from "../src/Battle.sol";
+
+contract BattleTest is Test {
+    Battle public battle;
+
+    uint256 INITIAL_PAYMENT_NEEDED = 0.01 ether;
+    address public PLAYER = makeAddr("player");
+    uint256 INITIAL_USER_BALANCE = 1 ether;
+    uint256 INITIAL_PAYMENT = 0.02 ether;
+    string CHARACTER_NAME = "toto";
+
+
+    function setUp() public {
+        battle = new Battle(INITIAL_PAYMENT_NEEDED);
+    }
+
+    function testGetCharacterName(string calldata _name) fundedPlayer public {
+        // It should returns an error if we provide an empty string
+        if (bytes(_name).length == 0) {
+            vm.expectRevert(Battle.Battle_InvalidName.selector);
+            battle.joinBattle{value: INITIAL_PAYMENT}(_name);
+            return;
+        }
+        battle.joinBattle{value: INITIAL_PAYMENT}(_name);
+        bytes32 expectedHashedCharacterName = keccak256(abi.encodePacked(_name));
+        bytes32 generatedHashedCharacterName = keccak256(abi.encodePacked(battle.getCharacterName(PLAYER)));
+        assertEq(expectedHashedCharacterName, generatedHashedCharacterName);
+    }
+
+    function testJoinBattleWithoutFund() fundedPlayer public {
+        uint256 valueToSend = INITIAL_PAYMENT_NEEDED - INITIAL_PAYMENT_NEEDED/2;
+        vm.expectRevert(Battle.Battle_NotEnoughInitialPayment.selector);
+        battle.joinBattle{value: valueToSend}(CHARACTER_NAME);
+    }
+    
+    function testWithdrawWealthIfNotPlayerShouldRevert() fundedPlayer public {
+        battle.joinBattle{value: INITIAL_PAYMENT}(CHARACTER_NAME);
+        address anotherPlayer = makeAddr("paul");
+        hoax(anotherPlayer, INITIAL_USER_BALANCE);
+        vm.expectRevert(Battle.Battle_AddressDoesntHaveCharacter.selector);
+        battle.withdrawWealth();
+    }
+
+    function testWithdrawWealthSuccessfull() fundedPlayer public {
+        battle.joinBattle{value: INITIAL_PAYMENT}(CHARACTER_NAME);
+
+        uint256 initialUserBalance = address(PLAYER).balance;
+        uint256 initialContractBalance = address(battle).balance;
+        uint256 expectedUserBalance = initialUserBalance + INITIAL_PAYMENT;
+        uint256 expectedContractBalance = initialContractBalance - INITIAL_PAYMENT;
+
+        vm.prank(PLAYER);
+        battle.withdrawWealth();
+
+        uint256 newUserBalance = address(PLAYER).balance;
+        uint256 newContractBalance = address(battle).balance;
+        assertEq(newUserBalance, expectedUserBalance);
+        assertEq(newContractBalance, expectedContractBalance);
+    }
+
+    function testGetCharacterWealth() fundedPlayer public {
+        uint256 wealthSent = INITIAL_PAYMENT+INITIAL_PAYMENT/10;
+        battle.joinBattle{value: wealthSent}(CHARACTER_NAME);
+        assertEq(battle.getCharacterWealth(PLAYER), wealthSent);
+    }
+
+    modifier fundedPlayer {
+        vm.prank(PLAYER);
+        vm.deal(PLAYER, INITIAL_USER_BALANCE);
+        _;
+    }
+}
